1ef6e86b5d015879086ccf32dd0f66c9632b85a0
IslamHarby <islam.ashour@elabs-corp.com>
2021-05-17 14:19:07 +0200

    add path image qrcode folder name

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -26,6 +26,6 @@ class Registration extends Model
 
     public function getOriginalPathAttribute()
     {
-        return env('APP_URL').$this->qrcode;
+        return env('APP_URL').'/public/qrcodes/'.$this->qrcode;
     }
 }
