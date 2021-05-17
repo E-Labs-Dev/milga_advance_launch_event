1b723c0882a72dfd56da132cd2292004fc5c0f39
IslamHarby <islam.ashour@elabs-corp.com>
2021-05-17 14:07:25 +0200

    edit path file email

--- resources/views/emails/registration.blade.php
+++ resources/views/emails/registration.blade.php
@@ -6,7 +6,7 @@
   <body>
     <h2>thank you for registration and below QR code will use it to attend event</h2>
 
-    <p><img src="{{$qrcode}}" width="100" height="100"></p>
+    <p><img src="%%qrcode%%" width="100" height="100"></p>
 
 </body>
 </html>
