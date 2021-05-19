4b11e0a3162ffb13deec4938d632e2a25f58f821
IslamHarby <islam.ashour@elabs-corp.com>
2021-05-19 15:06:25 +0200

    delete related with user and edit title and update style

--- resources/views/dashboard/games/index.blade.php
+++ resources/views/dashboard/games/index.blade.php
@@ -12,7 +12,7 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title">DataTable with default features</h3>
+            <h3 class="card-title">DataTable Users Play Games</h3>
         </div>
         <!-- /.card-header -->
         <div class="card-body">
