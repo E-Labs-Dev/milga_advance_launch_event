0f0cccd4e462b51ce4c84f2dc5a34bff3446f5a8
IslamHarby <islam.ashour@elabs-corp.com>
2021-06-06 12:35:52 +0200

    disabled btn when submit form and change word in dashboard

--- resources/views/dashboard/games/index.blade.php
+++ resources/views/dashboard/games/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title"> game scores</h3>
+            <h3 class="card-title"> games scores</h3>
         </div>
         <!-- /.card-header -->
         <div class="card-body">
