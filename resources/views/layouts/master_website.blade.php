0dbc2e216726fcf91d6857077b19fd79cce22fe4
IslamHarby <islam.ashour@elabs-corp.com>
2021-06-13 14:27:00 +0200

    csrf mismatch

--- resources/views/layouts/master_website.blade.php
+++ resources/views/layouts/master_website.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!--first mobile meta-->
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Milga</title>
 
